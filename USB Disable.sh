9456f3a49e77c6f7f0acaffd930044ee0e1170b2
SathyaRepo <132055343+SathyaRepo@users.noreply.github.com>
2023-04-30 11:03:03 +0530

    Delete USB Disable.sh

--- USB Disable.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Create a new rule to disable USB mass storage access
-echo 'ACTION=="add", SUBSYSTEMS=="usb", DRIVERS=="usb-storage", RUN+="/bin/sh -c '\''modprobe -r usb-storage'\''"' | sudo tee /etc/udev/rules.d/99-disable-usb-storage.rules
-
-# Reload the udev rules to apply the changes
-sudo udevadm control --reload-rules
-
-echo "USB mass storage access disabled successfully."
