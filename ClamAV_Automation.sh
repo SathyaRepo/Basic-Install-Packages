ff0a7701e249ab951e7df6718be55488195a738a
SathyaRepo <132055343+SathyaRepo@users.noreply.github.com>
2023-05-08 10:48:52 +0530

    ClamAV auto scan and update

---
+++ ClamAV_Automation.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Set the time for the daily scan
+SCAN_TIME="02:00"
+
+# Add a cron job to update the virus definitions every day at 1:00 AM
+echo "0 1 * * * /usr/bin/freshclam" | sudo tee -a /etc/crontab > /dev/null
+
+# Add a cron job to perform the daily scan at the specified time
+echo "0 ${SCAN_TIME} * * * root /usr/bin/clamscan -r /" | sudo tee -a /etc/crontab > /dev/null
+
+# Restart the cron service to apply the changes
+sudo service cron restart
